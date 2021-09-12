2553a8fca5f6d4668d1e0ca6b450fa1c7a293169
zhengqi0115 <90536946+zhengqi0115@users.noreply.github.com>
2021-09-12 10:05:05 +0800

    app_center_first

---
+++ files/排行/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(h,p),q,[r],s,_(t,u,v,w,g,x,y,_(),z,[],A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,M,M,N,O,P,null,Q,R,S,T,U,V,W,R,X,Y,_(G,H,I,Z),ba,R,bb,bc,_(bd,be,bf,bg,bh,bg,bi,bg,bj,bk,I,_(bl,bm,bn,bm,bo,bm,bp,bq)),i,_(j,k,l,m)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="排行.html",d="generationDate",e=new Date(1631411901570.68),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=375,l="height",m=667,n="adaptiveViews",o="sketchKeys",p="s0",q="variables",r="OnLoadVariable",s="page",t="packageId",u="8d586a36afa4458bbebf25734f3da578",v="type",w="Axure:Page",x="排行",y="notes",z="annotations",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="center",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageAlignment",M="near",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="borderVisibility",Y="borderFill",Z=0xFF797979,ba="cornerRadius",bb="cornerVisibility",bc="outerShadow",bd="on",be=false,bf="offsetX",bg=5,bh="offsetY",bi="blurRadius",bj="spread",bk=0,bl="r",bm=0,bn="g",bo="b",bp="a",bq=0.349019607843137,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
